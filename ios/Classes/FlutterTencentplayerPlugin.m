a35d1ad742e3e761a4b26088c48d09218a45bbf9
wilson <yxw_android@163.com>
2019-08-12 17:35:29 +0800

    update

--- ios/Classes/FlutterTencentplayerPlugin.m
+++ ios/Classes/FlutterTencentplayerPlugin.m
@@ -2,19 +2,28 @@
 
 @implementation FlutterTencentplayerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_tencentplayer"
-            binaryMessenger:[registrar messenger]];
-  FlutterTencentplayerPlugin* instance = [[FlutterTencentplayerPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_tencentplayer"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterTencentplayerPlugin* instance = [[FlutterTencentplayerPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
-
+    
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"init" isEqualToString:call.method]) {
+        NSLog(@"init");
+    }else if([@"create" isEqualToString:call.method]){
+        NSLog(@"create");
+    }else if([@"download" isEqualToString:call.method]){
+         NSLog(@"download");
+    }else if([@"stopDownload" isEqualToString:call.method]){
+         NSLog(@"stopDownload");
+    }else {
+        result(FlutterMethodNotImplemented);
+    }
+    
+    
+    
 }
-
+    
 @end
