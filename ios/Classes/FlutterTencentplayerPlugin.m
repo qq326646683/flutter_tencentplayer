b547069235c3ad77515cc32780fd385764a5cbff
wilson <yxw_android@163.com>
2019-10-22 16:44:20 +0800

    修复内存bug

--- ios/Classes/FlutterTencentplayerPlugin.m
+++ ios/Classes/FlutterTencentplayerPlugin.m
@@ -8,7 +8,7 @@
 
 @property(readonly, nonatomic) NSObject<FlutterTextureRegistry>* registry;
 @property(readonly, nonatomic) NSObject<FlutterBinaryMessenger>* messenger;
-@property(readonly, nonatomic) NSMutableDictionary* players;
+//@property(readonly, nonatomic) NSMutableDictionary* players;
 @property(readonly, nonatomic) NSMutableDictionary* downLoads;
 @property(readonly, nonatomic) NSObject<FlutterPluginRegistrar>* registrar;
 
@@ -17,10 +17,11 @@
 
 @end
 
+
 @implementation FlutterTencentplayerPlugin
 
 NSObject<FlutterPluginRegistrar>* mRegistrar;
-
+FLTVideoPlayer* player ;
 
 - (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     self = [super init];
@@ -28,8 +29,9 @@ NSObject<FlutterPluginRegistrar>* mRegistrar;
     _registry = [registrar textures];
     _messenger = [registrar messenger];
     _registrar = registrar;
-    _players = [NSMutableDictionary dictionaryWithCapacity:1];
+   // _players = [NSMutableDictionary dictionaryWithCapacity:1];
     _downLoads = [NSMutableDictionary dictionaryWithCapacity:1];
+     NSLog(@"FLTVideo  initWithRegistrar");
     return self;
 }
 
@@ -46,13 +48,16 @@ NSObject<FlutterPluginRegistrar>* mRegistrar;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+     //NSLog(@"FLTVideo  call name   %@",call.method);
     if ([@"init" isEqualToString:call.method]) {
         [self disposeAllPlayers];
         result(nil);
     }else if([@"create" isEqualToString:call.method]){
+        NSLog(@"FLTVideo  create");
+        [self disposeAllPlayers];
         FLTFrameUpdater* frameUpdater = [[FLTFrameUpdater alloc] initWithRegistry:_registry];
-        FLTVideoPlayer* player;
-        player = [[FLTVideoPlayer alloc] initWithCall:call frameUpdater:frameUpdater registry:_registry messenger:_messenger];
+//        FLTVideoPlayer*
+        player= [[FLTVideoPlayer alloc] initWithCall:call frameUpdater:frameUpdater registry:_registry messenger:_messenger];
         if (player) {
             [self onPlayerSetup:player frameUpdater:frameUpdater result:result];
         }
@@ -101,9 +106,8 @@ NSObject<FlutterPluginRegistrar>* mRegistrar;
     
     NSDictionary* argsMap = call.arguments;
     int64_t textureId = ((NSNumber*)argsMap[@"textureId"]).unsignedIntegerValue;
-    FLTVideoPlayer* player = _players[@(textureId)];
-    
-    
+//    FLTVideoPlayer* player = _players[@(textureId)];
+
     if([@"play" isEqualToString:call.method]){
         [player resume];
         result(nil);
@@ -130,9 +134,11 @@ NSObject<FlutterPluginRegistrar>* mRegistrar;
         [player setBitrateIndex:index];
         result(nil);
     }else if([@"dispose" isEqualToString:call.method]){
+         NSLog(@"FLTVideo  dispose   ----   ");
         [_registry unregisterTexture:textureId];
-        [_players removeObjectForKey:@(textureId)];
-        [player dispose];
+       // [_players removeObjectForKey:@(textureId)];
+        //_players= nil;
+        [self disposeAllPlayers];
         result(nil);
     }else{
         result(FlutterMethodNotImplemented);
@@ -143,22 +149,18 @@ NSObject<FlutterPluginRegistrar>* mRegistrar;
 - (void)onPlayerSetup:(FLTVideoPlayer*)player
          frameUpdater:(FLTFrameUpdater*)frameUpdater
                result:(FlutterResult)result {
-    _players[@(player.textureId)] = player;
+//    _players[@(player.textureId)] = player;
     result(@{@"textureId" : @(player.textureId)});
     
 }
 
 -(void) disposeAllPlayers{
-     NSLog(@" 初始化播放器状态----------");
+     NSLog(@"FLTVideo 初始化播放器状态----------");
     // Allow audio playback when the Ring/Silent switch is set to silent
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
-    
-    for (NSNumber* textureId in _players) {
-        [_registry unregisterTexture:[textureId unsignedIntegerValue]];
-        [[_players objectForKey:textureId] dispose];
+    if(player){
+        [player dispose];
+        player = nil;
     }
-    [_players removeAllObjects];
 }
-
-
 @end
